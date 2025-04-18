92474711dcdb0f55ba11463ee8cf9402f7203bc4
Jonathon Hill <jonathon@compwright.com>
2025-04-18 17:10:24 -0400

    Format currency field, copy changes

--- src/StoplightAnalyzer.svelte
+++ src/StoplightAnalyzer.svelte
@@ -1,6 +1,8 @@
 <svelte:options customElement="stoplight-analyzer" />
 
 <script>
+	import { currency as svelteCurrency } from 'svelte-currency';
+
 	const minLoanAmount = 10000;
 
 	const currency = new Intl.NumberFormat('en-US', {
@@ -55,23 +57,31 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,800,900&display=swap" media="all">
 </svelte:head>
 
-{#snippet depthCalc()}
+{#snippet valueCalc()}
 	<tbody>
 		<tr>
 			<td>As-Is Value <small>(0.7*ARV - rehab)</small></td>
 			<td class="text-end">{currency.format(asIsValue)}</td>
 		</tr>
-		<tr>
-			<td>Depth <small>(Purchase Price &div; As-is value)</small></td>
-			<td class="text-end">{depth.toFixed(1)}%</td>
-		</tr>
 	</tbody>
 {/snippet}
 
 {#snippet loanCalc()}
+	<tbody>
+		<tr>
+			<td>Downpayment</td>
+			<td class="text-end">
+				{#if downpaymentNeededAmount > 0}
+					{currency.format(downpaymentNeededAmount)}
+				{:else}
+					$0.00
+				{/if}
+			</td>
+		</tr>
+	</tbody>
 	<tbody class="table-group-divider">
 		<tr>
-			<td>Purchase <small>(80% of as-is)</small></td>
+			<td>Loan at Purchase <small>(80% of as-is)</small></td>
 			<td class="text-end">{currency.format(purchaseDraw)}</td>
 		</tr>
 		<tr>
@@ -81,22 +91,22 @@
 	</tbody>
 	<tfoot class="table-group-divider fw-bold">
 		<tr>
-			<td>Total loan amount</td>
+			<td>Total Loan Amount</td>
 			<td class="text-end">{currency.format(totalLoanAmount)}</td>
 		</tr>
 	</tfoot>
 {/snippet}
 
 <div data-bs-theme="light">
-	<div class="container mt-3 mb-5">
+	<form class="container mt-3 mb-5">
 		<nav class="nav nav-underline">
 			<div class="nav-item">
-				<button class="nav-link" class:active={rehabRequired} onclick={setRehabRequired}>
+				<button class="nav-link" class:active={rehabRequired} onclick={setRehabRequired} type="button">
 					Rehab Required
 				</button>
 			</div>
 			<div class="nav-item">
-				<button class="nav-link" class:active={!rehabRequired} onclick={setNoRehab}>
+				<button class="nav-link" class:active={!rehabRequired} onclick={setNoRehab} type="button">
 					No Rehab
 				</button>
 			</div>
@@ -107,40 +117,38 @@
 				<div class="input-group mb-3">
 					<span class="input-group-text">$</span>
 					<div class="form-floating">
-						<input placeholder="After Repair Value (ARV)" class="form-control" min={10000} type="number" step="1" id="arvInput" bind:value={arvAmount} />
+						<input use:svelteCurrency={{ currency: 'USD' }} placeholder="After Repair Value (ARV)" class="form-control" min={10000} type="number" step="1" id="arvInput" onkeyup={e => arvAmount = e.target.valueAsNumber} />
 						<label for="arvInput">After Repair Value (ARV)</label>
 					</div>
 				</div>
 				<div class="input-group mb-3">
 					<span class="input-group-text">$</span>
 					<div class="form-floating">
-						<input placeholder="Rehab Amount" class="form-control" min={10000} type="number" step="1" id="rehabInput" bind:value={rehabAmount} />
+						<input use:svelteCurrency={{ currency: 'USD' }} placeholder="Rehab Amount" class="form-control" min={10000} type="number" step="1" id="rehabInput" onkeyup={e => rehabAmount = e.target.valueAsNumber} />
 						<label for="rehabInput">Rehab Amount</label>
 					</div>
 				</div>
 				<div class="input-group">
 					<span class="input-group-text">$</span>
 					<div class="form-floating">
-						<input placeholder="Purchase Price" class="form-control" min={10000} type="number" step="1" id="purchaseInput" bind:value={purchaseAmount} />
+						<input use:svelteCurrency={{ currency: 'USD' }} placeholder="Purchase Price" class="form-control" min={10000} type="number" step="1" id="purchaseInput" onkeyup={e => purchaseAmount = e.target.valueAsNumber} />
 						<label for="purchaseInput">Purchase Price</label>
 					</div>
 				</div>
-				{#if totalLoanAmount > minLoanAmount}
+				{#if totalLoanAmount > minLoanAmount && rehabAmount > 0 && purchaseAmount > minLoanAmount}
 					{#if depth <= 80}
 						<div class="alert alert-success mt-4">
-							<b>Fully Fundable</b>&mdash;we can fund the entire purchase and rehab cost:
+							<b>Fully Fundable</b>&mdash;we can fund the entire purchase and rehab cost in draws:
 							<table class="table table-sm w-100 mx-auto mt-3 mb-2 table-success">
-								{@render depthCalc()}
+								{@render valueCalc()}
 								{@render loanCalc()}
 							</table>
 						</div>
 					{:else if depth <= 100}
 						<div class="alert alert-warning mt-4">
-							<b>Fundable with downpayment</b>&mdash;we can fund if you provide a
-							<b>{currency.format(downpaymentNeededAmount)}</b> downpayment
-							to bring our loan to 80% of as-is:
+							<b>Fundable with downpayment</b>&mdash;required to bring our loan to 80% of as-is:
 							<table class="table table-sm w-100 mx-auto mt-3 mb-2 table-warning">
-								{@render depthCalc()}
+								{@render valueCalc()}
 								{@render loanCalc()}
 							</table>
 						</div>
@@ -149,7 +157,7 @@
 							<b>Sorry, not fundable</b>&mdash;you're buying above the as-is value,
 							with a high chance of losing money.
 							<table class="table table-sm w-100 mx-auto mt-3 mb-2 table-danger">
-								{@render depthCalc()}
+								{@render valueCalc()}
 							</table>
 						</div>
 					{/if}
@@ -160,7 +168,7 @@
 				<div class="input-group">
 					<span class="input-group-text">$</span>
 					<div class="form-floating">
-						<input placeholder="Current Value" class="form-control" min={10000} type="number" step="1" id="noRehabValueInput" bind:value={noRehabValue} />
+						<input use:svelteCurrency={{ currency: 'USD' }} placeholder="Current Value" class="form-control" min={10000} type="number" step="1" id="noRehabValueInput" onkeyup={e => noRehabValue = e.target.valueAsNumber} />
 						<label for="noRehabValueInput">Current Value</label>
 					</div>
 				</div>
@@ -173,13 +181,13 @@
 			</div>
 		{/if}
 
-		<div class="text-center">
-			<button class="btn btn-outline-secondary btn-sm mx-2" onclick={clearScenario}>
+		<div class="text-center pb-5">
+			<button class="btn btn-outline-secondary btn-sm mx-2" type="reset" onclick={clearScenario}>
 				Clear Scenario
 			</button>
-			<button class="btn btn-outline-secondary btn-sm mx-2" onclick={setExample}>
+			<button class="btn btn-outline-secondary btn-sm mx-2" type="reset" onclick={setExample}>
 				Show Example
 			</button>
 		</div>
-	</div>
+	</form>
 </div>
